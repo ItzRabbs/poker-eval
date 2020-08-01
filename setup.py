926d5141a9cbd915e1d18917962430311ae3b473
ItzRabbs <maxim.rabinovich1@gmail.com>
2020-08-01 11:27:24 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='poker-eval',
+    version='1.0.0',
+    description='Poker odds evaluator',
+    author='Maxim Rabinovich',
+    author_email='maxim.rabinovich1@gmail.com',
+    packages=['poker-eval'],  # same as name
+    install_requires=[],  # external packages as dependencies
+)
